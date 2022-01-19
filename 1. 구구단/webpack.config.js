20f92f4c4576c65c5af457bb8a80a40f9e72cd0e
Lowheads <false9@kakao.com>
2022-01-19 18:44:30 +0900

    리액트 웹팩 설정 및 클래스형 숫자야구

---
+++ 1. 구구단/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval', // hidden-source-map
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+
+  entry: {
+    app: './client',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'], // browserslist
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+        plugins: [],
+      },
+    }],
+  },
+  plugins: [
+    // new webpack.LoaderOptionsPlugin({ debug: true }),
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'), // __dirname = 현재 폴더, 'dist' = 경로
+  },
+};
